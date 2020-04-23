7ebc0d838b2065882ad9f244b401a9fb61c6151c
Damon Ross <damonphilipross@gmail.com>
2020-04-24 00:28:03 +1200

    got the dern daterberse werking

--- app/models/models.py
+++ app/models/models.py
@@ -1,7 +1,7 @@
 from app.run import db
 
-class Model(db.Model):
-  name = db.Column(db.string(400), unique=False, nullable=False, primary_key=True)
+class ModelPrototype(db.Model):
+  name = db.Column(db.String(80), unique=False, nullable=False, primary_key=True)
 
   def __repr__(self):
     return '<Name: {}>'.format(self.title)
--- app/run.py
+++ app/run.py
@@ -3,27 +3,27 @@ from flask import render_template
 from flask import Flask
 from flask import redirect
 from flask_sqlalchemy import SQLAlchemy
-from models.models import Model
+from models.models import ModelPrototype
 
 import os
-
-
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = 'sqlite:///{}'.format(
     os.path.join(project_dir, "database.db"))
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
   if request.form:
-    new_item = Model(name=request.form.get('name'))
+    new_item = ModelPrototype(name=request.form.get('name'))
     db.session.add(new_item)
-    print(request.form)
-  return render_template('index.html')
+  db_things = ModelPrototype.query.all()
+  return render_template("home.html", db_things=db_things)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
