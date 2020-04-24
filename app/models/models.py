441078ac587d0cb038491579e5f2131ba85e1201
Damon Ross <damonphilipross@gmail.com>
2020-04-25 11:22:03 +1200

    added in db

--- app/models/models.py
+++
@@ -1,7 +0,0 @@
-from app.run import db
-
-class ModelPrototype(db.Model):
-  name = db.Column(db.String(80), unique=False, nullable=False, primary_key=True)
-
-  def __repr__(self):
-    return '<Name: {}>'.format(self.title)
