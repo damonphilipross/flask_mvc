aa462c73c0ca3af87af25f8a78a70abbd54d8c4d
Damon Ross <damonphilipross@gmail.com>
2020-04-25 20:19:41 +1200

    removed flask boot strap cuz it don't work

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object(Config)
