380bffe1a949b55b3128fcf87507012c8026fe55
Damon Ross <damonphilipross@gmail.com>
2020-04-26 01:47:21 +1200

    cleared stuff up

--- app/models/models.py
+++ app/models/models.py
@@ -3,7 +3,7 @@ from app import db
 
 class ModelPrototype(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
+    name = db.Column(db.String(100), index=True, unique=True)
 
     def __repr__(self):
         return '<Name {}>'.format(self.name)
